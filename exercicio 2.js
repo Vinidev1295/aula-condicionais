f66d28a9e2371de068a3edcc1a1e78df256ad600
Vinidev1295 <vinidias1295@gmail.com>
2024-09-30 11:08:58 -0300

    Create exercicio 2.js

---
+++ exercicio 2.js
@@ -0,0 +1,16 @@
+let verificar = prompt("Voce estuda em qual turno M/V/N?")
+let matutino = "M"
+let vespertino = "V"
+let noturno = "N"
+if(verificar==="M"){
+    console.log("Bom Dia!")
+}else{
+}
+if(verificar==="V"){
+console.log("Boa Tarde!")
+}else{
+}
+if(verificar==="N"){
+console.log("Boa Noite!")
+}else{
+}
