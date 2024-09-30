60b88d0dce94174fec9d4df8b8517b184c6d00d9
Vinidev1295 <vinidias1295@gmail.com>
2024-09-30 11:24:01 -0300

    Create exercicio 3.js

---
+++ exercicio 3.js
@@ -0,0 +1,15 @@
+let turno = prompt("Voce estuda em qual turno? ")
+switch(turno){
+  case 'M':
+  console.log("Bom dia!")
+  break
+  case 'V':
+  console.log("Boa tarde!")
+  break   
+  case 'N':
+  console.log("Boa Noite")
+  break
+  default:
+    console.log("O turno que você escolheu não existe.")
+  break
+}
