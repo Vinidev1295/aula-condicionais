78b8a5b8967bcb2b1bd33bb3ad1fee67d6ab840c
Vinidev1295 <vinidias1295@gmail.com>
2024-09-30 11:30:20 -0300

    Create exercicio 4.js

---
+++ exercicio 4.js
@@ -0,0 +1,7 @@
+let filme = prompt("Qual genêro de filme você irá assistir? ")
+let ing = prompt("Qual o preço do ingresso? ")
+if(filme==="fantasia"&&ing<15){
+console.log("Bom filme")
+}else{
+    console.log("Escolha outro filme!")
+}
